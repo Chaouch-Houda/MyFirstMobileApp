18bc2513fb5b3212492d07d54d5efb341e6dbaff
HoudaChaouch <houdachaouch05@gmail.com>
2024-10-08 00:24:27 +0100

    initial commit

---
+++ app/(tabs)/bootmark.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Bootmark = () => {
+  return (
+    <View>
+      <Text>Bootmark</Text>
+    </View>
+  )
+}
+
+export default Bootmark
